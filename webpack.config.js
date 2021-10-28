7ceb03ddf371a8cf9341ad2b042cc0b343dee1d8
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-10-28 21:43:34 +0100

    Making changes on Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: {
-     index: './src/index.js',
-     print: './src/print.js',
-   },
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devServer: {
+
+    static: './dist',
+
+  },
   plugins: [
+
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+
+      template: './src/index.html',
+
     }),
+
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
- };
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
